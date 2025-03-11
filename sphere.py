765a1b5695be923dece6fefb3217f215f511e218
thepipboy <3476647793@qq.com>
2025-03-11 17:57:55 +0800

    Add files via upload

---
+++ sphere.py
@@ -0,0 +1,76 @@
+from sympy import polyhedron
+import trimesh
+import math
+def Hexadecimal():
+    a = "00001011"
+    b = "00001100"
+    c = "00001101"
+    d = "00001110"
+    e = "00001111"
+    f = "00010000"  
+class polyhedron():
+    def __init__polyhedron(Vertex,Edges,faces):
+        self.Vertex = [V]
+        self.Edges = [E]
+        self.faces = [F]
+        this.Edges = this.Radius
+        return V - E + F == 2
+    
+    def __init__Surface(Tetrahedron,Hexhedron,Octhedron,Dodecahedron,Icosahedron): 
+             this.Edges = 1
+             self.Tetrahedron = math.sqrt(3) * [Edges]**2
+     
+             self.Hexhedron = 6 * [Edges]**2
+    
+             self.Octahedron = 2 * math.sqrt(3) * [Edges]**2
+    
+             self.Dodecahedron = 3 * math.sqrt(25 + 10 * math.sqrt(5)) * [Edges]**2
+    
+             self.Icosahedron = 5 * math.sqrt(3) * [Edges]**2
+
+    return Surface
+        
+    def __init__Volume(Tetrahedron,Hexhedron,Octhedron,Dodecahedron,Icosahedron):
+             this.Radius = 1
+             self.Tetrahedron = 8 * math.sqrt(3) * [Radius]/27
+
+             self.Hexhedron = 8 * math.sqrt(3) * [Radius]/9
+
+             self.Octahedron = (4/3) * [Radius]
+
+             self.Dodecahedron = [15 + 7 * math.sqrt(5)]/4 * [Radius]
+
+             self.Tetrahedron = math.sqrt[25 + 10 * math.sqrt(5)]/4 * [Radius]
+    
+    return Volume
+    
+class sphere(Surface,Volume):
+    def space(X,Y,Z,Radius,theta,fai):
+         this.X = Radius * math.cos(theta)
+         this.Y = Radius * math.sin(fai)
+         this.Z = Radius * math.cos(theta) * math.cos(fai)
+    def time(x,y,z,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12):
+        return [
+            a * sin(x + t1) + b * cos(x - t1) + c * sin(y + t2) + d * cos(y - t2) + e * sin(z + t3) + f * cos(z - t3),       #t123
+            a * asin(x + t4) + b * acos(x - t4) + c * asin(y + t5) + d * acos(y - t5) + e * asin(z + t6) + f * acos(z - t6), #t456
+            a * sinh(x + t7) + b * cosh(x - t7) + c * sinh(y + t8) + d * cosh(y - t8) + e * sinh(z + t9) + f * cosh(z - t9), #t789
+            a * tan(x + t10) + b * atan(x - t10) + c * tan(y + t11) + d * atan(y - t11) + e * tan(z + t12) + f * atan(z - t12) #t101112
+        ]   
+   
+
+    def __init__abcdef(dotline,Tetrahedron,Hexhedron,Octhedron,Dodecahedron,Icosahedron):
+        this.dot = 0
+        this.line = 1
+        self.a = dotline
+        self.b = Tetrahedron
+        self.c = Hexhedron
+        self.d = Octahedron
+        self.e = Dodecahedron
+        self.f = Icosahedron
+
+        return t1t2[a + b + c + d + e + f]
+        return t3t4[a - b - c - d - e - f]
+        return t5t6[a * b * c * d * e * f]
+        return t7t8[a / b / c / d / e / f]
+        return t9t10[a % b % c % d % e % f]
+        
