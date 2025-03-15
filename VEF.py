22dcc05943b43f1f57512e5812cbfd524cb767e4
thepipboy <3476647793@qq.com>
2025-03-15 18:43:40 +0800

    Update and rename sphere.py to VEF.py

---
+++ VEF.py
@@ -0,0 +1,47 @@
+from sympy import polyhedron
+import trimesh
+import math
+def Hexadecimal():
+    a = "00001011"
+    b = "00001100"
+    c = "00001101"
+    d = "00001110"
+    e = "00001111"
+    f = "00010000"  
+class polyhedron(Surface,Volume):
+    def __init__polyhedron(Vertex,Edges,Faces):
+        Vertex = [V]
+        Edges = [E]
+        Faces = [F]
+        this.Edges = this.Radius
+        return V - E + F == 2
+    
+    def __init__Surface(Tetrahedron,Hexhedron,Octhedron,Dodecahedron,Icosahedron): 
+             this.Edges = 1
+             Tetrahedron = 8 * math.sqrt(3) / 3 * [Edges]
+     
+             Hexhedron = 8 * [Edges]
+    
+             Octahedron = 4 * math.sqrt(3) * [Edges]
+    
+             Dodecahedron = 3 * math.sqrt(25 + 10 * math.sqrt(5)) * [Edges]
+    
+             Icosahedron = [30 * math.sqrt(3) - 10 * math.sqrt(15)] * [Edges]
+
+             return Surface
+        
+    def __init__Volume(Tetrahedron,Hexhedron,Octhedron,Dodecahedron,Icosahedron):
+             this.Radius = 1
+             Tetrahedron = 8 * math.sqrt(3) * [Radius]/27
+
+             Hexhedron = 8 * math.sqrt(3) * [Radius]/9
+
+             Octahedron = (4/3) * [Radius]
+
+             Dodecahedron = [15 + 7 * math.sqrt(5)]/4 * [Radius]
+
+             Tetrahedron = math.sqrt[25 + 10 * math.sqrt(5)]/4 * [Radius]
+    
+             return Volume
+
+        
